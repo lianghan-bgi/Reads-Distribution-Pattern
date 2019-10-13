e85949b827441ed436ac50ac536ab45559ce6947
lianghan-bgi <56495838+lianghan-bgi@users.noreply.github.com>
2019-10-13 13:10:02 +0800

    Add files via upload

---
+++ data.sh
@@ -0,0 +1,3 @@
+perl ./script/data.pl ./HEALTH-BGI.list 10 > ./HEALTH-BGI.list.txt
+perl ./script/data.pl ./LIVER-BGI.list 10 > ./LIVER-BGI.list.txt
+perl ./script/data.pl ./LUNG-BGI.list 10 > ./LUNG-BGI.list.txt
